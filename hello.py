c648ec1a86530f9acab0970670cbde14b04eac5d
SaudShaik <soudshaik18@gmail.com>
2026-01-01 22:20:54 +0530

    Day-1: Learning Python

---
+++ hello.py
@@ -0,0 +1,27 @@
+print("Hello World!")
+
+a=10
+b=20
+sum=a+b
+print("Sum of a + b is:", sum)
+
+# program to ask user's input
+
+name=input("Enter Name :")
+print("Hello",name,"Welcome to my world!")
+
+#Understand the behavior of numbers
+number1=5
+print(type(number1))
+
+number2=34.5
+print(type(number2))
+
+number3=input("Enter a Number")
+print(type(number3))
+
+#number3 will return str type, but we can change it to integer type
+
+number4=int(input("Enter a Numebr"))
+print(number4)
+print(type(number4))
