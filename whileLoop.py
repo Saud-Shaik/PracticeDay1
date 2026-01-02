2d9323770b4ee571341163fb97a1c4074ffdd815
SaudShaik <soudshaik18@gmail.com>
2026-01-02 11:54:55 +0530

    Day:2-Learning Python

---
+++ whileLoop.py
@@ -0,0 +1,20 @@
+#while loop syntax
+# while test_condition:
+#      statement(s)
+
+count=0
+while count < 5:
+    print("I am inside a loop")
+    count+=1
+
+count1=5
+while count1 <=10:
+    print(count1)
+    count1+=1
+
+number=int(input("Enter a number"))
+count2=1
+while count2 <=10:
+ product = number * count2
+ print(product)
+ count2+=1
